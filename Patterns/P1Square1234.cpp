2c5c0c75ff4a31f732d631b3c2090ab3edfeb2e4
pratapshingane18 <sushrutrao465@gmail.com>
2023-06-25 08:44:35 +0530

    Patterns P1

--- Patterns/P1Square1234.cpp
+++ Patterns/P1Square1234.cpp
@@ -1,9 +1,38 @@
 #include<iostream>
 using namespace std;
 
+/*
 
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+*/
 int main(){
 
-    
+    int r,c;
+    cin>>r>>c;
+
+    for(int i =0; i < r; i++){
+        for(int j=0; j < c; j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
     return 0;
 }
+
+
+/*
+Input:
+3 4
+
+Output:
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+*/
